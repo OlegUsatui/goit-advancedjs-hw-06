14a5db50afd58398589fe6ecdbe417df928ff24f
oleg-usatyi <oleg_usatyi@ukr.net>
2024-08-10 22:47:42 +0200

    Base and generic types

--- src/generic/7.ts
+++ src/generic/7.ts
@@ -9,8 +9,9 @@ export enum UserRole {
   guest = 'guest',
 }
 
+type RoleDescriptionT = Record<UserRole, string>;
 // Замініть наступний код на версію за допомогою Record
-const RoleDescription = {
+const RoleDescription: RoleDescriptionT = {
   admin: 'Admin User',
   editor: 'Editor User',
   guest: 'Guest User',
